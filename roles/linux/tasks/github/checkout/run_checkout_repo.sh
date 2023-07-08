6db236be03ff6ab7472bc34cbc051ebeb3239d50
VenkiSpace <112202207+VenkiSpace@users.noreply.github.com>
2023-07-08 21:26:02 +0530

    Update run_checkout_repo.sh

--- roles/linux/tasks/github/checkout/run_checkout_repo.sh
+++ roles/linux/tasks/github/checkout/run_checkout_repo.sh
@@ -1,1 +1,1 @@
-ansible-playbook checkout_repo.yml --extra-vars="groupId='888' groupName='g1'"
+ansible-playbook checkout_repo.yml --extra-vars="dest_dir='/etc/temp/' repo='VenkiSpace/ansible.git' git_token='***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***'"
